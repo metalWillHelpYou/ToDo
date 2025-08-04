c53e06bdba3551a7903c0d9fa8ad8e328f04e42c
Daniil Krivonogov <113389092+metalWillHelpYou@users.noreply.github.com>
2025-08-04 17:05:48 +0300

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by metalWillHelpYou on 04.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
